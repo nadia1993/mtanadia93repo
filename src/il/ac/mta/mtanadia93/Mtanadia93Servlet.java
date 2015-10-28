c920bd4d1192f35c8e7fb2f8aa5b504096583df3
nadia1993 <nadiamed1993@gmail.com>
2015-10-28 17:47:42 +0200

    COMMIT 1

---
+++ src/il/ac/mta/mtanadia93/Mtanadia93Servlet.java
@@ -0,0 +1,11 @@
+package il.ac.mta.mtanadia93;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Mtanadia93Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
